b99205b36e06efc5ab3f519a4314d8d92be2adf4
ayns <peenats1203@gmail.com>
2019-03-25 08:40:52 -0700

    Create Crawl

---
+++ Crawl.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+let home = FileManager.default.homeDirectoryForCurrentUser
+let path = "Swift/"
+let swiftUrl = home.appendingPathComponent(path)
+let fileManager = FileManager.default
+var isDirectory: ObjCBool = false
+fileManager.fileExists(atPath: swiftUrl.path, isDirectory: &isDirectory)
+
+if( isDirectory.boolValue ) {
+    
+    do{
+        let contentURLs = try fileManager.contentsOfDirectory(at: swiftUrl, includingPropertiesForKeys: [.nameKey, .fileSizeKey], options: .skipsHiddenFiles)
+        
+        for fileURL in contentURLs {
+            do {
+                let fileAttributes = try fileURL.resourceValues(forKeys:[.nameKey, .fileSizeKey])
+                print(fileAttributes.name!)
+            } catch { print(error, fileURL) }
+        }
+    } catch { print(error)
+}
+    
+} else {
+    
+    print("Error!! File doesn't exists")
+}
