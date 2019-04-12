1fef8f8aa60d46056f139070c67851639c0c61fd
ayns <peenats1203@gmail.com>
2019-04-11 23:18:50 -0700

    Add to DP

--- DP/MakeOne/main.swift
+++ DP/MakeOne/main.swift
@@ -14,10 +14,24 @@
 // Print the minimum number of operations performed.
 // (Hint: 10 -> 9 -> 3 -> 1)
 
-
-
-
 import Foundation
+
+func minOperations(n: Int) -> Int {
+    var d = [Int](repeating: 0, count: 100)
+    d[1] = 0
+    for i in 2...n {
+        d[i] = d[i-1] + 1 // subtract 1
+        if (i % 2 == 0 && d[i] > d[i/2] + 1) {
+            d[i] = d[i/2] + 1 // divide by 2
+        }
+        if (i % 3 == 0 && d[i] > d[i/3] + 1) {
+            d[i] = d[i/3] + 1 // divide by 3
+        }
+    }
+    return d[n]
+}
+
+print(minOperations(n: 10))
 
 
 
---
+++ DP/NonDecreasingDigits/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  NonDecreasingDigits
+//
+//  Created by 酒井綾菜 on 2019-04-11.
+//  Copyright © 2019 Ayana Sakai. All rights reserved.
+//
+// A Number with digits in non-decreasing order: 2234, 3678, 11119
+// These numbers are not in non-decreasing order: 2232, 3676, 91111
+// Given a length of digits, write a program to print the total number of non-decreasing digits.
+// (A number can start with 0)
+// N (1 <= N <= 1000): the number of digits (length)
+// Print the number of non-decreasing digits numbers.
+
+import Foundation
+
+
